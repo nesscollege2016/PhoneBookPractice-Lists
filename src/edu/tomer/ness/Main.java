1222fbda98889e8d7835c6da7a4f67752c32f46b
nesscollege2016 <nesscollege2016@gmail.com>
2016-03-21 12:05:26 +0200

    Grades Employees ArrayLists

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -1,16 +1,50 @@
 package edu.tomer.ness;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        PhoneBook book = new PhoneBook();
+        ArrayList<String> employees = new ArrayList<>();
+        ArrayList<String> phones = new ArrayList<>();
+        ArrayList<Integer> grades = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            book.insertContact("Moe " + i, "Shmoe", "05050");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter name: ");
+            employees.add(scanner.next());
+
+            System.out.println("Enter phone: ");
+            String phone = scanner.next();
+            phones.add(phone);
+
+            //Validate the Input: 0-100
+            //input the grade
+            System.out.println("Enter grade: ");
+            int grade = scanner.nextInt();
+
+            //While grade is not in range: Input again:
+            while (grade < 0 || grade >100){
+                System.out.println("Grade must be between 0-100");
+                grade = scanner.nextInt();
+            }
+            grades.add(grade);
+
+            System.out.println("Continue? true/false");
+            boolean shouldCountinue = scanner.nextBoolean();
+            if (!shouldCountinue){
+                break;
+            }
         }
 
-        book.dispDetails(10);
-        book.dispDetails("M");
-        //book.dispAllContacts();
+
+        for (int i = 0; i < employees.size(); i++) {
+            System.out.println("Name: " + employees.get(i));
+            System.out.println("Phone: " + phones.get(i));
+            System.out.println("grade: " + grades.get(i));
+        }
+
     }
 }
